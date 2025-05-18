0d54d5c4a77ae0d1d96271bc08d813119e15fdf7
AkingDsq <3241208202@qq.com>
2025-05-18 17:02:13 +0800

    Reapply "Delete APP/MIMO/build"
    This reverts commit 90d782a0e12f089de061f3538809e40cda45a878.

--- APP/MIMO/build/Qt_6_8_2_Clang_arm64_v8a-Debug/_deps/android_openssl-src/tst_openssl/tst_openssl.pro
+++
@@ -1,9 +0,0 @@
-QT += testlib gui network
-
-CONFIG += qt warn_on depend_includepath testcase
-
-TEMPLATE = app
-
-SOURCES +=  tst_openssl.cpp
-
-android: include(../openssl.pri)
--- APP/MIMO/build/Qt_6_8_2_Clang_arm64_v8a-Debug/appMIMO.bat
+++
@@ -1,6 +0,0 @@
-E:/Qt/6.8.2/mingw_64/bin/qt-android-runner.py ^
-    --adb "E:/Qt/Android/Sdk/platform-tools/adb" ^
-    --build-path "D:/git/work/ESP32-WROOM-32/APP/MIMO/build/Qt_6_8_2_Clang_arm64_v8a-Debug/android-build-appMIMO" ^
-    --apk "D:/git/work/ESP32-WROOM-32/APP/MIMO/build/Qt_6_8_2_Clang_arm64_v8a-Debug/android-build-appMIMO/appMIMO.apk" ^
-    %*
-
